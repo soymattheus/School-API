2691202545f77e07a597a0b14dc4f72b590acc38
Matheus Tavares <matheustavares1a@gmail.com>
2025-02-13 22:33:06 -0300

    adjustments to environmental variables

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
 
+print(os.getenv("DB_HOST_DOCKER"))
+print(os.getenv("DB_HOST"))
+print(os.getenv("DB_PORT"))
+print(os.getenv("DB_NAME"))
+print(os.getenv("DB_USER"))
+print(os.getenv("DB_PASSWORD"))
+
 class Config:
     # Detecta se está rodando dentro de um container Docker
     if os.path.exists("/.dockerenv"):
-        DB_HOST = os.getenv("DB_HOST", "host.docker.internal")  # Docker
+        DB_HOST = os.getenv("DB_HOST_DOCKER", "host.docker.internal")  # Docker
     else:
         DB_HOST = os.getenv("DB_HOST", "127.0.0.1")  # Localhost
 
